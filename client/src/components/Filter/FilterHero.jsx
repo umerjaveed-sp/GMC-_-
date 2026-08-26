2dd14650731cb16ada3c4035069b28c6b3e4802c
umer javeed <umersp6@gmail.com>
2026-08-26 10:40:43 +0500

    Filter Hero Done!

--- client/src/components/Filter/FilterHero.jsx
+++ client/src/components/Filter/FilterHero.jsx
@@ -3,15 +3,18 @@ import skyline from "../../assets/filters/skyline2.jpg";
 
 const FilterHero = () => {
   return (
-    <div className="mt-[70px]">
+    <div className="mt-[30px] md:mt-[70px]">
       <div
-        className="w-auto h-[552px]  bg-slate-600  bg-cover bg-center bg-no-repeat flex items-center justify-center"
+        className="w-auto  min-h-[300px] md:min-h-[400px] lg:h-[552px] bg-slate-600 bg-cover bg-center bg-no-repeat flex items-center justify-center px-4 sm:px-6 lg:px-8"
         style={{ backgroundImage: `url(${skyline})` }}
       >
-        <div className="text-white flex items-center flex-col justify-center text-[56px] font-semibold">
-          {" "}
-          <div>Explore Thousands of Programs</div>
-          <div>across the World </div>
+        <div className="text-white flex items-center flex-col justify-center text-center">
+          <div className="text-[22px] sm:text-[36px] md:text-[44px] lg:text-[56px] font-semibold leading-tight">
+            Explore Thousands of Programs
+          </div>
+          <div className="text-[22px] sm:text-[36px] md:text-[44px] lg:text-[56px] font-semibold leading-tight">
+            across the World
+          </div>
         </div>
       </div>
     </div>
